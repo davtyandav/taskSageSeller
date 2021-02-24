1d27e974ce75f462f2eacd7531915e4e0965f1ae
DavidDavtyan <davit.davtyan84@gmail.com>
2021-02-24 14:41:56 +0400

    first commit

---
+++ src/com/github/davdavtyan/Main.java
@@ -0,0 +1,107 @@
+package com.github.davdavtyan;
+
+import java.util.*;
+
+public class Main {
+
+    public static final Map<String, String> urls = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Utils.SELECT_OPTION_DOWNLOAD_ADDRESS);
+        System.out.println(Utils.FOR_LOAD_YOUR_SELECTED_TEXT);
+        System.out.println(Utils.FOR_LOAD_RANDOM_TEXT);
+        System.out.println(Utils.FOR_GET_ORIGINAL_ADDRESS);
+        System.out.println(Utils.EXIT);
+        boolean isStart = true;
+        while (isStart) {
+            int i;
+            if (!sc.hasNextInt()) {
+                System.err.println(Utils.BEAD_DATA);
+                sc.next();
+                i = -1;
+            } else {
+                i = sc.nextInt();
+            }
+
+            if (i == 1) {
+                System.out.println(Utils.ENTER_ORIGINAL_ADDRESS);
+                String originUrl = sc.next();
+
+                System.out.println(Utils.ENTER_TEXT_FOR_SHORT_ADDRESS);
+                String next = sc.next();
+
+                String loadUrl = loadUrl(originUrl, next);
+                if (loadUrl.equals(Utils.BEAD_ADDRESS)) {
+                    System.err.println(Utils.BEAD_ADDRESS);
+                } else {
+                    System.out.println(loadUrl);
+                }
+                i = -1;
+
+            } else if (i == 2) {
+                System.out.println(Utils.ENTER_ORIGINAL_ADDRESS);
+                String originUrl = sc.next();
+
+                System.out.println(loadRandomUrl(originUrl));
+                i = -1;
+            } else if (i == 3) {
+                System.out.println(Utils.ENTER_SHORT_ADDRESS);
+                String shortUrl = sc.next();
+                String url = getUrl(shortUrl);
+                if (url.equals(Utils.BEAD_ADDRESS)) {
+                    System.err.println(Utils.BEAD_ADDRESS);
+                } else {
+                    System.out.println(url);
+                }
+                i = -1;
+            } else if (i == 0) {
+                isStart = false;
+            }
+            if (i == -1) {
+                System.out.println(Utils.SELECT_OPTION_DOWNLOAD_ADDRESS);
+            }
+        }
+
+    }
+
+    public static String loadUrl(String originUrl, String keyword) {
+        if (keyword.length() > 20 || !URLValidator.isValid(originUrl)) {
+            return Utils.BEAD_DATA;
+        }
+        return createUrl(originUrl, keyword);
+    }
+
+    public static String loadRandomUrl(String originUrl) {
+        return createUrl(originUrl, loadRandomTextForUrl());
+    }
+
+    private static String loadRandomTextForUrl() {
+        StringBuilder sb = new StringBuilder(5);
+
+        for (int i = 0; i < 5; i++) {
+            int index = (int) (Utils.ALPHA_NUMERIC_TEXT.length() * Math.random());
+            sb.append(Utils.ALPHA_NUMERIC_TEXT.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    private static String createUrl(String originUrl, String sb) {
+        int i = originUrl.indexOf("//");
+        String substring = originUrl.substring(0, i + 2);
+        String url = substring + "short.en/" + sb;
+
+        urls.put(originUrl, url);
+        return url;
+    }
+
+    public static String getUrl(String shortUrl) {
+        Map.Entry<String, String> stringStringEntry = urls.entrySet().stream().filter(t -> t.getValue().equals(shortUrl)).findFirst().orElse(null);
+        if (stringStringEntry != null) {
+            return stringStringEntry.getKey();
+        } else {
+            return Utils.BEAD_ADDRESS;
+        }
+    }
+
+}
---
+++ src/com/github/davdavtyan/Utils.java
@@ -0,0 +1,16 @@
+package com.github.davdavtyan;
+
+public class Utils {
+
+    public static final String ENTER_ORIGINAL_ADDRESS = "Введите оригинальное адрес";
+    public static final String ENTER_SHORT_ADDRESS = "Введите короткий адрес";
+    public static final String ENTER_TEXT_FOR_SHORT_ADDRESS = "Введите страку для кароткого адреса";
+    public static final String BEAD_ADDRESS = "неправильное адрес";
+    public static final String BEAD_DATA = "некоректные данные";
+    public static final String SELECT_OPTION_DOWNLOAD_ADDRESS = "Выберите вариант загрузки адреса";
+    public static final String ALPHA_NUMERIC_TEXT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+    public static final String FOR_LOAD_YOUR_SELECTED_TEXT = "1:  для загрски с вашей вибранной стракой";
+    public static final String FOR_LOAD_RANDOM_TEXT = "2:  для загрски с рандомной стракой";
+    public static final String FOR_GET_ORIGINAL_ADDRESS = "3:  для получения оригиналного адреса";
+    public static final String EXIT = "0:  для выхода";
+}
